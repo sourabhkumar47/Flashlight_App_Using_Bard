9e26a43252ea06c5500eccdc0bf803baa3e901a2
Sourabh Kumar <20010001030@gateway.edu.in>
2023-05-26 00:56:45 +0530

    fixed bugs

--- app/src/main/java/com/example/flashlight_app_using_bard/MainActivity.kt
+++ app/src/main/java/com/example/flashlight_app_using_bard/MainActivity.kt
@@ -1,46 +1,104 @@
 package com.example.flashlight_app_using_bard
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.flashlight_app_using_bard.ui.theme.Flashlight_App_Using_BardTheme
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Flashlight_App_Using_BardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Surface(color = MaterialTheme.colors.background) {
+                FlashlightApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FlashlightApp() {
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    var isFlashlightOn by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    if (isFlashlightOn) {
+                        turnOffFlashlight(context)
+                    } else {
+                        turnOnFlashlight(context)
+                    }
+                    isFlashlightOn = !isFlashlightOn
+                }
+            },
+            modifier = Modifier
+                .size(120.dp)
+                .background(if (isFlashlightOn) Color.Red else Color.Green),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Transparent,
+                contentColor = MaterialTheme.colors.onBackground
+            )
+        ) {
+            Text(text = if (isFlashlightOn) "OFF" else "ON")
+        }
+    }
+}
+
+private fun turnOnFlashlight(context: Context) {
+    val packageManager = context.packageManager
+    val cameraManager = ContextCompat.getSystemService(context, CameraManager::class.java)
+    if (packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH) && cameraManager != null) {
+        try {
+            val cameraId = cameraManager.cameraIdList[0]
+            cameraManager.setTorchMode(cameraId, true)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
+
+private fun turnOffFlashlight(context: Context) {
+    val packageManager = context.packageManager
+    val cameraManager = ContextCompat.getSystemService(context, CameraManager::class.java)
+    if (packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH) && cameraManager != null) {
+        try {
+            val cameraId = cameraManager.cameraIdList[0]
+            cameraManager.setTorchMode(cameraId, false)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Flashlight_App_Using_BardTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview() {
+    FlashlightApp()
 }
